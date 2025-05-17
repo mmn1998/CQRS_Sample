97a5c36e30ca1cd72fc0086ac6ccbe94171d20e2
mmn1998 <mm.nomani.1998@gmail.com>
2025-05-17 09:24:27 +0330

    .

---
+++ src/Application/CQRS_Sample.Application/Extensions/RegisterApplicationServicesExtension.cs
@@ -0,0 +1,34 @@
+﻿using CQRS_Sample.Application.Commands.Handlers.SomeModels;
+using CQRS_Sample.Application.Queries.Handlers.SomeModels;
+using CQRS_Sample.Common.MediatRHelpers;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CQRS_Sample.Application.Extensions;
+
+public static class RegisterApplicationServicesExtension
+{
+    public static IServiceCollection RegisterApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Scan(scan =>
+            scan.FromAssemblyOf<SomeModelEventHandler>()
+                .AddClasses(classes => classes.AssignableTo(typeof(INotificationHandler<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+        services.Scan(scan =>
+            scan.FromAssemblyOf<SomeModelQueryHandler>()
+                .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+        services.Scan(scan =>
+            scan.FromAssemblyOf<SomeModelEventHandler>()
+                .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+        return services;
+    }
+}
---
+++ src/Application/CQRS_Sample.Application/Queries/Handlers/SomeModels/SomeModelQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using CQRS_Sample.Application.Contracts.Models.Queries.SomeModels;
+using CQRS_Sample.Common.MediatRHelpers;
+using CQRS_Sample.Persistence.Query.Repositories.SomeModels;
+
+namespace CQRS_Sample.Application.Queries.Handlers.SomeModels;
+
+public class SomeModelQueryHandler : IQueryHandler<GetSomeModelQuery, GetSomeModelQueryResult>,
+    IQueryHandler<GetAllSomeModelsQuery, IEnumerable<GetSomeModelQueryResult>>
+{
+    private readonly ISomeModelQueryRepository _repository;
+
+    public SomeModelQueryHandler(ISomeModelQueryRepository repository)
+    {
+        _repository = repository;
+    }
+    public async Task<GetSomeModelQueryResult> Handle(GetSomeModelQuery request, CancellationToken cancellationToken)
+    {
+        return await _repository.GetById(request);
+    }
+
+    public async Task<IEnumerable<GetSomeModelQueryResult>> Handle(GetAllSomeModelsQuery request, CancellationToken cancellationToken)
+    {
+        return await _repository.GetAll(request);
+    }
+}
