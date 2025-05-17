65c4179a7c0fdb2db68adf661b6ec778cedf34f1
mmn1998 <mm.nomani.1998@gmail.com>
2025-05-17 11:24:31 +0330

    .

---
+++ src/Presentation/CQRS_Sample.API/Controllers/SomeModelsController.cs
@@ -0,0 +1,46 @@
+﻿using CQRS_Sample.Application.Contracts.Models.Commands.SomeModels;
+using CQRS_Sample.Application.Contracts.Models.Queries.SomeModels;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS_Sample.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SomeModelsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SomeModelsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet]
+    public async Task<object> Get()
+    {
+        var query = new GetAllSomeModelsQuery();
+        return await _mediator.Send(query);
+    }
+    [HttpGet("{id}")]
+    public async Task<object> Get([FromRoute] long id)
+    {
+        var query = new GetSomeModelQuery { Id = id };
+        return await _mediator.Send(query);
+    }
+    [HttpDelete("{id}")]
+    public async Task<object> Delete([FromRoute] long id)
+    {
+        var command = new DeleteSomeModelCommand { Id = id };
+        return await _mediator.Send(command);
+    }
+    [HttpPost]
+    public async Task<object> Post([FromBody] CreateSomeModelCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+    [HttpPut]
+    public async Task<object> Put([FromBody] ModifySomeModelCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+}
